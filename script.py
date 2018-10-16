d42bcac82a6933ef33ae7edd52998c4e450d2cb4
Tomasz <umpawlis@myumanitoba.ca>
2018-10-16 14:01:47 -0500

    Started Creating Cleaned Up Script
    Initial script commit

---
+++ script.py
@@ -0,0 +1,194 @@
+from Bio import Phylo
+import os.path
+import multiset
+import numpy as np
+import xlsxwriter
+import matplotlib.pyplot as plt
+
+newickFileName = 'Anc27_subtree.dnd'
+strains = []
+ancestralCounter = 0
+
+######################################################
+# Strain
+# Parameters:
+# Description: Stores information about a Strain from the newick tree
+######################################################
+class Strain(object):
+    name = ""
+    sequence = []
+    genes = []
+    descendants = []
+    operonPositions = []
+    singletonDict = {}
+    trackingEvents = []
+    hasData = False
+
+    #Class constructor
+    def __init__(self, name, sequence, descendants, operonPositions, singletonDict):
+        self.name = name
+        self.sequence = sequence
+        self.descendants = descendants
+        self.operonPositions = operonPositions
+        self.singletonDict = singletonDict
+
+    #Prints the Strain content
+    def printStrain(self):
+        print("{ Name: %s, Sequence: %s, Descendants: %s }" %(self.name, self.sequence, self.descendants))
+
+    def getName(self):
+        return self.name
+    def getSequence(self):
+        return self.sequence
+    def getDescendants(self):
+        return self.descendants
+    def getGenes(self):
+        return self.genes
+    def setGenes(self, genes):
+        self.genes = genes
+    def getOperonPositions(self):
+        return self.operonPositions
+    def getSingletonDict(self):
+        return self.singletonDict
+    def getTrackingEvents(self):
+        return self.trackingEvents
+    def setTrackingEvents(self, trackingEvents):
+        self.trackingEvents = trackingEvents
+    def setHasData(self, hasData):
+        self.hasData = hasData
+    def getHasData(self):
+        return self.hasData
+    
+######################################################
+# processFileSequence
+# Parameters: sequence - sequence to get the operons from
+# Description: extracts a list of operons from a sequence
+######################################################
+def processFileSequence(sequence):
+    geneList = []
+    operonList = []
+    singletonList = {}
+    index = 0
+    geneIndex = 0
+    operonPositions = []
+
+    while index < len(sequence):
+        #Operon
+        if (sequence[index] == '[') or (sequence[index] == '-' and sequence[index + 1] == '['):
+            startIndex = index
+            operonPositions.append(geneIndex)
+
+            while sequence[index] != ']':
+                if sequence[index] == ',':
+                    geneIndex += 1
+                index += 1
+
+            #increment the index to include the ]
+            index += 1
+            operonList.append(sequence[startIndex:index])
+            if sequence[startIndex] == '[':
+                geneList.extend([gene.strip() for gene in sequence[startIndex+1:index-1].split(',')])
+            else:
+                geneList.extend([gene.strip() for gene in sequence[startIndex+2:index-1].split(',')])
+                
+        #Singleton
+        if index < len(sequence) and sequence[index] == ',':
+            geneIndex += 1
+            if sequence[index+2] != '[' and sequence[index+2] != '<' and sequence[index+3] != '[':
+                index += 1
+                startIndex = index
+
+                while sequence[index] != ',' and index < len(sequence)-1:
+                    index += 1
+
+                #Add singleton gene
+                operonList.append(sequence[startIndex:index])
+                operonPositions.append(geneIndex)
+
+                singletonList[str(geneIndex)] = sequence[startIndex:index]
+                geneList.append(sequence[startIndex:index].strip().replace("-", ""))
+                index -= 1
+        index += 1
+
+    return operonList, operonPositions, singletonList, geneList
+    
+######################################################
+# traverseNewickTree
+# Parameters: node - The node that we want to process
+# Description: Traverses a provided newick tree and reads in sequences if any (uses post traversal)
+######################################################
+def traverseNewickTree(node):
+    #Global variables
+    global strains
+    global ancestralCounter
+    #Local variables
+    leftChildStrain = None
+    rightChildStrain = None
+
+    #Check if the clade has children
+    if len(node.clades) > 0:
+        leftChildStrain = traverseNewickTree(node.clades[0])
+        if len(node.clades) > 1:
+            rightChildStrain = traverseNewickTree(node.clades[1])
+
+    #Check if the clade has a name, if it does, check if it has a directory for its sequence
+    if node.name is not None and len(node.name) > 0:
+        if os.path.isdir(node.name):
+            if os.path.isfile(node.name + '/sequence.txt'):
+                fileGeneSequence = open(node.name + '/sequence.txt', 'r').read()
+                operons, operonPositions, singletonDict, allGenes = processFileSequence(fileGeneSequence)
+
+                #Random newline characters in file are causing the alignment scores to be messed up
+                for x in range(0, len(operons)):
+                    operons[x] = (operons[x]).replace('\r', '').replace('\n', '')
+                for x in range(0, len(allGenes)):
+                    allGenes[x] = (allGenes[x]).replace('\r', '').replace('\n', '')
+                for key in singletonDict.keys():
+                    singletonDict[key] = (singletonDict[key]).replace('\r', '').replace('\n', '')
+
+                strain = Strain(node.name, operons, [], operonPositions, singletonDict)
+                strain.setGenes(allGenes)
+                strain.setHasData(True)
+                strains.append(strain)
+                
+                return strain
+            
+            else:
+                print('No sequence file found for node: %s' % node.name)
+        else:
+            print('No directory found for node: %s' % node.name)
+
+    if leftChildStrain is not None and leftChildStrain.getHasData() and rightChildStrain is not None and rightChildStrain.getHasData():
+        print('These are the strains being compared: %s, %s'%(leftChildStrain.getName(), rightChildStrain.getName()))
+        ancestralOperons, trackingEvents = processStrains(leftChildStrain, rightChildStrain)        
+        
+        ancestralCounter += 1
+        node.name = 'Ancestor %s' % (ancestralCounter)
+        ancestor = Strain('Ancestor %s' % (ancestralCounter), ancestralOperons, [leftChildStrain.getName(), rightChildStrain.getName()], [], {})
+        ancestor.setTrackingEvents(trackingEvents)
+        ancestor.setHasData(False)
+        strains.append(ancestor)
+        
+        return ancestor
+
+    #If the left child has a sequence, return it
+    elif leftChildStrain is not None and leftChildStrain.getHasData():
+        return leftChildStrain
+
+    #If the right child has a sequence, return it
+    elif rightChildStrain is not None and rightChildStrain.getHasData():
+        return rightChildStrain
+
+    #If neither has a sequence, return None
+    else:
+        return None
+    
+######################################################
+#                       main
+######################################################
+print('Reading in newick tree from file: %s...' % (newickFileName))
+newickTree = Phylo.read(newickFileName, 'newick')  
+Phylo.draw(newickTree)
+
+#Traverses the newick tree to reconstruct the ancestral genomes
+result = traverseNewickTree(newickTree.clade)
