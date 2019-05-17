4939589ac5f1dc0a518e7ca17f913320e99eae39
Tomasz <umpawlis@myumanitoba.ca>
2019-05-17 14:45:12 -0500

    Added region details and counts to the bacterial strain

--- Refactorization/BacterialStrain.py
+++ Refactorization/BacterialStrain.py
@@ -4,20 +4,31 @@
 # Description: Stores information about the specific bacterial strain
 ######################################################
 class BacterialStrain(object):
-
     #Class constructor
     def __init__(self, name, genomeFragments):
         self.name = name
         self.genomeFragments = genomeFragments
+
         self.codonMismatchDetails = 'Codon Mismatch:'
         self.substitutionDetails = 'Substitution:'
+
         self.duplicationCounts = {}
         self.duplicationDetails = 'Duplication:'
+
         self.deletionCounts = {}
         self.deletionDetails = 'Deletion:'
-        
+
+        self.inversionCounts = {}
+        self.inversionDetails= 'Inversion:'
+
+        self.transpositionCounts = {}
+        self.transpositionDetails = 'Transposition:'
+
+        self.invertedTranspositionCounts = {}
+        self.invertedTranspositionDetails = 'Inverted Transposition:'
+
     def addCodonMismatchDetails(self, codonMismatchDetails):
         self.codonMismatchDetails += codonMismatchDetails
-        
+
     def addSubstitutionDetails(self, substitutionDetails):
         self.substitutionDetails += substitutionDetails
