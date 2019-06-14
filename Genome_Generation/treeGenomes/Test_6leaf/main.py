3012a2affce1f796ab13c09cf31c8ea924fb2893
Tomasz <umpawlis@myumanitoba.ca>
2019-06-14 11:23:41 -0500

    updated tree name file

--- Genome_Generation/treeGenomes/Test_6leaf/main.py
+++ Genome_Generation/treeGenomes/Test_6leaf/main.py
@@ -25,7 +25,7 @@ from FragmentService import determineAncestralFragmentArrangementWithoutNeighbor
 from SequenceService import updateGlobalInvertedTranspositionSizeDistributionCounter
 
 #Application parameters
-newickFileName = 'tree4Leaf.dnd' #Name of newick tree file
+newickFileName = 'tree6Leaf.dnd' #Name of newick tree file
 
 #Global vaiables used by the script
 strains = [] #Global variable that stores all of the strains in the phylogenetic tree
